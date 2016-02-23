4ec5f072f77e930a8c6ae73d588f96b2fae8c25a
Dyjesse18 <dyjesse18@saintjosephhs.com>
2016-02-23 09:24:06 -0500

    Create NewGui.java

---
+++ NewGui.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.swing.*;
+import java.awt.event.*;
+
+//lets create a window first
+public class NewGui
+{
+    //variables
+    private static JFrame myFrame;
+    
+    
+    public static void main(String[] args) 
+    {
+        
+    }//end of main
+}//end of NewGui
